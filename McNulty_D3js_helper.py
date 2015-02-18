f669d1c471235e9438ab62b7ef7be06a100c4915
Emmanuele <emmanuele.salvati@gmail.com>
2015-02-18 17:33:43 -0500

    add two buttons to bar chart. Complete

--- McNulty_D3js_helper.py
+++ McNulty_D3js_helper.py
@@ -6,7 +6,8 @@ from McNulty_feature_selection import retrieve_dataframe
 if __name__ == '__main__':
     men = retrieve_dataframe()
 
-    cp_men = men['chest_pain_type'].value_counts()
-    with open('my.tsv', 'w') as tsv:
+    cp_men = men['diagnosis'].value_counts()
+    with open('diag.tsv', 'w') as tsv:
+
         for ind in cp_men.index:
             tsv.write('%s\t%s\n' % (ind, cp_men[ind]))
